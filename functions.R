0df27ba147d52a3a0fb3b1b3c66341e12ec3bfd8
sherrillmix <sherrillmix@users.noreply.github.com>
2021-10-14 14:53:17 -0400

    Clean up dependencies

--- functions.R
+++ functions.R
@@ -1,18 +1,3 @@
-runStan<-function(countTab,mod,iter=2000){
-  stan_sample <- rstan::sampling(
-    mod,
-    data=list(
-      counts=countTab,
-      nTime=ncol(countTab),
-      nLineage=nrow(countTab)
-    ),
-    iter=iter,
-    chains=50,
-    control=list(max_treedepth=15),
-  )
-  return(stan_sample)
-}
-
 calcCredInt<-function(stan_sample,quants=c(.025,.975),names=NULL,colPattern='^props\\['){
   mat<-as.matrix(stan_sample)
   props<-mat[,grepl(colPattern,colnames(mat))]
@@ -27,17 +12,6 @@ calcCredInt<-function(stan_sample,quants=c(.025,.975),names=NULL,colPattern='^pr
   upper<-convertToMat(meanLowerUpper[3,])
   rownames(means)<-rownames(lower)<-rownames(upper)<-names
   return(list('mean'=means,'upper'=upper,'lower'=lower))
-}
-
-calcDensity<-function(stan_sample,names=NULL){
-  mat<-as.matrix(stan_sample)
-  drops<-mat[,grepl('^dropChange\\[',colnames(mat))]
-  vaccines<-mat[,grepl('^vaccineChange\\[',colnames(mat))]
-  minMax<-range(cbind(drops,vaccines))
-  vaccineDensity<-apply(vaccines,2,density,from=minMax[1],to=minMax[2])
-  dropDensity<-apply(drops,2,density,from=minMax[1],to=minMax[2])
-  names(vaccineDensity)<-names(dropDensity)<-names
-  return(list('vaccine'=vaccineDensity,'drop'=dropDensity))
 }
 
 plotMeanUpperLower<-function(means,upper,lower,countTab,baseDate,cols=NULL,...){
@@ -65,33 +39,10 @@ plotBars<-function(countTab,baseDate,cols=NULL,showLegend=TRUE,showSum=TRUE,show
   plot(1,1,type='n',xlim=c(1,ncol(countTab))+c(-1,1),ylim=c(0,1),las=1,xlab='',bty='l',xaxt='n',yaxs='i',ylab='Proportion',xaxs='i')
   for(ii in 1:ncol(propTab))rect(ii-.5,cumsum(propTab[,ii]),ii+.5,cumsum(c(0,propTab[-nrow(propTab),ii])),col=cols[rownames(propTab)],lwd=.5)
   uniqDate<-data.frame('week'=which(counts>0),'rdate'=baseDate+(which(counts>0)-1)*7)
-  if(is.null(skipDate))dnar::slantAxis(1,uniqDate$week,if(showDates)sub('  +',' ',format(uniqDate$rdate,'%b %e %Y')) else rep('',nrow(uniqDate)),location=.5,cex=dateCex)
-  else dnar::slantAxis(1,uniqDate$week,ifelse(1:nrow(uniqDate)%%skipDate==1,sub('  +',' ',format(uniqDate$rdate,'%b %e %Y')),''),location=.5,cex=dateCex)
-  if(showSum)dnar::slantAxis(3,which(counts>0),counts[counts>0],location=.4,cex=.6,axisArgs=list(lwd=NA,lwd.tick=1),textOffsets=-.1)
+  if(is.null(skipDate))slantAxis(1,uniqDate$week,if(showDates)sub('  +',' ',format(uniqDate$rdate,'%b %e %Y')) else rep('',nrow(uniqDate)),location=.5,cex=dateCex)
+  else slantAxis(1,uniqDate$week,ifelse(1:nrow(uniqDate)%%skipDate==1,sub('  +',' ',format(uniqDate$rdate,'%b %e %Y')),''),location=.5,cex=dateCex)
+  if(showSum)slantAxis(3,which(counts>0),counts[counts>0],location=.4,cex=.6,axisArgs=list(lwd=NA,lwd.tick=1),textOffsets=-.1)
   if(showLegend)legend('bottom',names(cols),fill=cols,inset=-.33,ncol=ceiling(nrow(countTab)/2),xpd=NA,cex=.8)
-}
-
-
-runStan2<-function(countTab,dropTab,vaccineTab,mod,iter=2000){#,hospitalTab
-  #should do more double checking
-  stan_sample <- rstan::sampling(
-    mod,
-    data=list(
-      counts=countTab,
-      #hospital=hospitalTab,
-      drop=dropTab,
-      vaccine=vaccineTab,
-      nTime=ncol(countTab),
-      nLineage=nrow(countTab)
-    ),
-    iter=iter,
-    chains=50,
-    thin=2,
-    control=list(max_treedepth=15),
-    pars=c('means','propsDrop','propsVaccine'),
-    include=FALSE
-  )
-  return(stan_sample)
 }
 
 plotIndivStan<-function(means,upper,lower,countTab,baseDate,cols=NULL,nCol=5,additional=NULL){
@@ -106,7 +57,7 @@ plotIndivStan<-function(means,upper,lower,countTab,baseDate,cols=NULL,nCol=5,add
     prettyY<-c(0,.5,1)
     if(ii %% nCol==1)axis(2,las=1,prettyY)
     else axis(2,prettyY,rep('',length(prettyY)))
-    if(ii > nrow(countTab)-nCol) dnar::slantAxis(1,(prettyDates-baseDate)/7+1,sub('  +',' ',format(prettyDates,'%b %e %Y')),cex=.8,tcl=-.2,location=.5)
+    if(ii > nrow(countTab)-nCol) slantAxis(1,(prettyDates-baseDate)/7+1,sub('  +',' ',format(prettyDates,'%b %e %Y')),cex=.8,tcl=-.2,location=.5)
     else axis(1,(prettyDates-baseDate)/7+1,rep('',length(prettyDates)),tcl=-.2)
     if(ii==(floor(nrow(means)/nCol/2)*nCol+1))mtext('Estimated proportion',2,line=2.2,at=1)
     title(main=rownames(countTab)[ii],line=-1.3,cex=.9)
@@ -131,7 +82,7 @@ plotIndivDense<-function(dense,cols=NULL,xlim=range(exp(dense[[1]]$x)),ylab='Fol
     plot(1,1,type='n',xlim=xlim,ylim=c(0,yMax),las=1,xlab='',bty='n',xaxt='n',yaxs='i',xaxs='i',yaxt='n',xaxt='n',log='x')
     polygon(exp(dense[[ii]]$x),dense[[ii]]$y,col=cols[names(dense)[ii]])
     title(main=names(dense)[ii],line=0,cex=.9)
-    dnar::logAxis(1,axisVals=c(-2,0,2,4))
+    logAxis(1,axisVals=c(-2,0,2,4))
     abline(v=1,lty=2)
     #if(ii==(2*nCol+1))mtext(2,line=2.2,at=par('usr')[4])
   }
@@ -139,49 +90,8 @@ plotIndivDense<-function(dense,cols=NULL,xlim=range(exp(dense[[1]]$x)),ylab='Fol
   text(grconvertX(.5,'ndc','user'),grconvertY(.02,'ndc','user'),ylab,xpd=NA,cex=1.2)
 }
 
-#deprecated
-plotStan<-function(stan_sample,countTab,baseDate,cols=NULL){
-  if(is.null(cols)) cols<-structure(c('#469990','#e6194B', '#4363d8', '#ffe119', '#f58231', '#42d4f4', '#3cb44b','#469990','#800000','#808000','#911eb4')[1:nrow(countTab)],.Names=rownames(countTab))
-  cols<-cols[names(cols) %in% rownames(countTab)]
-  counts<-apply(countTab,2,sum)
-  propTab<-apply(countTab,2,function(xx)xx/sum(xx))
-  mat<-as.matrix(stan_sample)
-  props<-mat[,grepl('^props\\[',colnames(mat))]
-  meanLowerUpper<-apply(props,2,meanCrI)
-  convertToMat<-function(xx){
-    row<-as.numeric(sub('[^0-9]+([0-9]+),[0-9]+.*','\\1',names(xx)))
-    col<-as.numeric(sub('[^0-9]+[0-9]+,([0-9]+).*','\\1',names(xx)))
-    tapply(xx,list(row,col),c)
-  }
-  means<-convertToMat(meanLowerUpper[1,])
-  lower<-convertToMat(meanLowerUpper[2,])
-  upper<-convertToMat(meanLowerUpper[3,])
-  #https://sashamaps.net/docs/resources/20-colors/
-  par(mar=c(3,3,.5,4),tcl=-.2,mgp=c(2,.3,0))
-  layout(matrix(c(1,0,2,0),ncol=1),height=c(1,.01,1,.15))
-  plot(1,1,type='n',xlim=c(1,ncol(countTab))+c(-1,1),ylim=c(0,1),las=1,xlab='',bty='l',xaxt='n',yaxs='i',ylab='Estimated proportion',xaxs='i')
-  for(ii in 1:nrow(means)){
-    polygon(c(1:ncol(means),ncol(means):1),c(lower[ii,],rev(upper[ii,])),border=NA,col=sprintf('%s33',cols[rownames(countTab)[ii]]))
-  }
-  for(ii in 1:nrow(means)){
-    lines(1:ncol(means),means[ii,],col=cols[rownames(countTab)[ii]])
-  }
-  abline(h=0)
-  text(ncol(means)+.5,means[,ncol(means)],rownames(countTab),cex=.7,xpd=NA,adj=0)
-  prettyDates<-lubridate::mdy(c('4/1/20','7/1/20','10/1/20','1/1/21','4/1/21'))
-  axis(1,(prettyDates-baseDate)/7+1,sub('  +',' ',format(prettyDates,'%b %e %Y')),cex=.8,tcl=-.5,mgp=c(3,.7,0))
-  #
-  plot(1,1,type='n',xlim=c(1,ncol(countTab))+c(-1,1),ylim=c(0,1),las=1,xlab='',bty='l',xaxt='n',yaxs='i',ylab='Proportion',xaxs='i')
-  for(ii in 1:ncol(propTab))rect(ii-.5,cumsum(propTab[,ii]),ii+.5,cumsum(c(0,propTab[-nrow(propTab),ii])),col=cols[rownames(propTab)])
-  uniqDate<-data.frame('week'=which(counts>0),'rdate'=baseDate+(which(counts>0)-1)*7)
-  dnar::slantAxis(1,uniqDate$week,sub('  +',' ',format(uniqDate$rdate,'%b %e %Y')),location=.5,cex=.6)
-  dnar::slantAxis(3,which(counts>0),counts[counts>0],location=.4,cex=.6,axisArgs=list(lwd=NA,lwd.tick=1),textOffsets=-.1)
-  legend('bottom',names(cols),fill=cols,inset=-.33,ncol=ceiling(nrow(countTab)/2),xpd=NA,cex=.8)
-  invisible(return(list(means,lower,upper)))
-}
 
-runStan3<-function(countTab,dropTab,vaccineTab,greek,mod,iter=2000){
-  #should do more double checking
+runStan<-function(countTab,dropTab,vaccineTab,greek,mod,iter=2000){
   if(length(greek)!=nrow(countTab))stop('Sublineage groupings not same length as count table')
   abundantGreek<-names(table(greek)[table(greek)>1])
   greekIds<-structure(1:(length(abundantGreek)+1),.Names=c('__BASE__',abundantGreek))
@@ -207,7 +117,7 @@ runStan3<-function(countTab,dropTab,vaccineTab,greek,mod,iter=2000){
   return(list('stan'=stan_sample,'dat'=dat,'greek'=greekIds,'lineage'=rownames(countTab)))
 }
 
-calcDensity2<-function(stan_sample,names=NULL,greekNames=NULL){
+calcDensity<-function(stan_sample,names=NULL,greekNames=NULL){
   mat<-as.matrix(stan_sample)
   drops<-mat[,grepl('^dropChange\\[',colnames(mat))]
   vaccines<-mat[,grepl('^vaccineChangeWithSub\\[',colnames(mat))]
@@ -282,4 +192,45 @@ plotMeanUpperLowerLog<-function(means,upper,lower,countTab,baseDate,cols=NULL,..
     prettyDates<-lubridate::mdy(c('4/1/20','7/1/20','10/1/20','1/1/21','4/1/21'))
     axis(1,(prettyDates-baseDate)/7+1,sub('  +',' ',format(prettyDates,'%b %e %Y')),cex=.8,tcl=-.5,mgp=c(3,.7,0))
   }
+}
+
+slantAxis<-function(side,at,labels=at,srt=ifelse(side %in% c(1,4),-45,45),location=1.2,adj=ifelse(side==2,1,0),axisArgs=list(),textOffsets=rep(0,length(at)),...){
+  do.call(graphics::axis,c(list(side,at,label=FALSE),axisArgs))
+  if(side %in% c(1,3)){
+    graphics::text(at+textOffsets, convertLineToUser(location,side), srt = srt, adj = adj, labels = labels, xpd = NA,...)
+  }else{
+    graphics::text(convertLineToUser(location,side),at+textOffsets, srt = srt, adj = adj, labels = labels, xpd = NA,...)
+  }
+  return(invisible(NULL))
+}
+
+logAxis<-function(side=2,exponent=TRUE,addExtra=!exponent,minorTcl=-.2,axisMin=-Inf,axisMax=Inf,offset=0,col.ticks='black',axisVals=NULL,...){
+  if(side %in% c(2,4)) parX<-sort(graphics::par('usr')[3:4])
+  else parX<-sort(graphics::par('usr')[1:2])
+  minX<-max(10^parX[1],axisMin)
+  maxX<-min(10^parX[2],axisMax)
+  if(log10(maxX)-log10(minX)>400)stop(simpleError('Huge range in logged axis'))
+  allTicks<-unlist(lapply(floor(log10(minX)):ceiling(log10(maxX)),function(x)1:9*10^x))
+  allTicks<-allTicks[allTicks<=maxX & allTicks>=minX]
+  graphics::axis(side,allTicks+offset,rep('',length(allTicks)),tcl=minorTcl,col.ticks=col.ticks)
+  if(ceiling(log10(minX))<=floor(log10(maxX)))prettyY<-seq(ceiling(log10(minX)),floor(log10(maxX)),1)
+  else prettyY<-c()
+  graphics::axis(side,10^prettyY+offset,rep('',length(prettyY)),tcl=minorTcl*2,col.ticks=col.ticks)
+  if(length(prettyY)>7)prettyY<-pretty(prettyY)
+  if(length(prettyY)==0)prettyY<-c(ceiling(log10(minX)),floor(log10(maxX)))
+  if(addExtra){
+    origPretty<-prettyY
+    if(sum(prettyY>=log10(minX)&prettyY<=log10(maxX))<4)prettyY<-unique(c(prettyY,origPretty+log10(5),origPretty-log10(10/5)))
+    if(sum(prettyY>=log10(minX)&prettyY<=log10(maxX))<4)prettyY<-unique(c(prettyY,origPretty+log10(2),origPretty-log10(10/2)))
+    if(sum(prettyY>=log10(minX)&prettyY<=log10(maxX))<4)prettyY<-unique(c(prettyY,origPretty+log10(3),origPretty-log10(10/3)))
+    if(sum(prettyY>=log10(minX)&prettyY<=log10(maxX))<4)prettyY<-unique(c(prettyY,origPretty+log10(7),origPretty-log10(10/7)))
+  }
+  if(exponent){
+    if(any(prettyY%%1!=0))labs<-sapply(prettyY,function(x)as.expression(bquote(.(10^(x%%1))%*%10^.(floor(x)))))
+    else labs<-ifelse(prettyY==0,1,sapply(prettyY,function(x)as.expression(bquote(10^.(floor(x))))))
+  }
+  else labs<-10^prettyY
+  if(is.null(axisVals))axisVals<-prettyY
+  graphics::axis(side,10^prettyY+offset,ifelse(prettyY %in% axisVals,labs,''),col.ticks=col.ticks,...)
+  return(invisible(list('minor'=allTicks,'major'=10^prettyY)))
 }
